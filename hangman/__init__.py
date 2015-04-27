f178f45698d53de76938fbe1042eceb18653bc0b
Jessica Stewart <jessica.stewart@adroll.com>
2015-04-27 16:02:16 -0700

    Update __init__.py

--- hangman/__init__.py
+++ hangman/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 hangman_app = Flask(__name__)
 
-hangman_app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/jessicastewart/projects/hangman/hangman.db'
+hangman_app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/YOUR_COMPS_USERNAME/projects/hangman/hangman/hangman.db'
 hangman_app.config['SECRET_KEY'] = '678297'
 hangman_app.config['DEBUG'] = True
 
