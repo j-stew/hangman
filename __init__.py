a3dea0c091c72630adbe81de49fb4d6f23ab119e
Jessica Stewart <jessica.stewart@adroll.com>
2015-04-27 10:45:54 -0700

    Delete __init__.py

--- __init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-hangman_app = Flask(__name__)
-
-hangman_app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/jessicastewart/projects/hangman/hangman.db'
-hangman_app.config['SECRET_KEY'] = '678297'
-hangman_app.config['DEBUG'] = True
-
-from hangman import model, controller
