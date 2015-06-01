dbf2996318d0b7184c75d306df94326684984302
Jessica Stewart <jessica.stewart@adroll.com>
2015-06-01 12:48:51 -0700

    changing db conn string to env var

--- hangman/__init__.py
+++ hangman/__init__.py
@@ -1,8 +1,9 @@
+import os
 from flask import Flask
 
 hangman_app = Flask(__name__)
 
-hangman_app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://jessicastewart:r0llr0ll123@localhost:5432/hangman'
+hangman_app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 hangman_app.config['SECRET_KEY'] = '678297'
 hangman_app.config['DEBUG'] = True
 
