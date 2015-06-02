328d36efb5e40fb58871897385cb48bb5377e83b
Jessica Stewart <jessica.stewart@adroll.com>
2015-06-02 14:46:25 -0700

    making debug mode env variable

--- hangman/__init__.py
+++ hangman/__init__.py
@@ -5,6 +5,6 @@ hangman_app = Flask(__name__)
 
 hangman_app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 hangman_app.config['SECRET_KEY'] = '678297'
-hangman_app.config['DEBUG'] = True
+hangman_app.config['DEBUG'] = os.environ['DEBUG']
 
 from hangman import model, controller
