e19f5104fd08583955c0c08ef259046b40981cb8
jessicastewart <jessica.stewart@adroll.com>
2015-06-08 08:45:29 -0700

    crazy big refactor that creates guess object and moves data from session to model

--- hangman/controller.py
+++ hangman/controller.py
@@ -17,70 +17,35 @@ def auth(f):
 		return f(*args, **kwargs)
 	return wrapper
 
-def validate_input(input):
-	if request.form.get(input) == None:
-		flash("Please input {}".format(input))
-	else:
-		return request.form.get(input)
-
-def random_word():
-	num = randint(1, model.Word.query.count()+1)
-	return model.Word.query.filter_by(id=num).first()
-
-def char_replace(guessed, guess, answer):
-	guessed = guessed.split(" ")
-	char_positions = [i for i, char in enumerate(answer) if char == guess]
-	for position in char_positions:
-		guessed[position] = guess
-	return " ".join(guessed)
-
-def add_game(game_type, guessed, answer, username):
-	game = model.Game(game_type, guessed, answer, username)
-	model.db.session.add(game)
-	user = model.User.query.filter_by(username=session['username']).first()
-	if game_type == 'loss':
-		user.loses += 1
-	else:
-		user.wins += 1
-	model.db.session.commit()
-
 ############
 ###ROUTES###
 ############
-
 @hangman_app.route("/login", methods=["GET", "POST"])
 def login():
 	if request.method == 'GET':
 		return render_template('login.html')
 
-	username = validate_input('username')
-	password = validate_input('password')
+	username, password = request.form.get('username'), request.form.get('password')
 
-	user = model.User.query.filter_by(username=username).first()
-	if not user:
+	if not model.User.get_user(username):
 		flash("Username not found. Please create account.")
 		return redirect(url_for('signup'))
 
-	user_password = user.password
-
-	if user_password != password:
+	if model.User.get_user(username).password != password:
 	 	flash("Password invalid. Please try again.")
 	 	return redirect(url_for('login'))
-	else:
-		session['username'] = username
-		return redirect(url_for('play'))
+
+	session['username'] = username
+	return redirect(url_for('play'))
 
 @hangman_app.route("/signup", methods=["GET", "POST"])
 def signup():
 	if request.method == 'GET':
 		return render_template('signup.html')
 
-	username = validate_input('username')
-	password = validate_input('password')
+	username, password = request.form.get('username'), request.form.get('password')
 
-	db_username = model.User.query.filter_by(username=username).first()
-
-	if db_username:
+	if model.User.get_user(username):
 		flash("Username already exists. Please try again.")
 		return redirect(url_for('signup'))
 	else:
@@ -98,58 +63,60 @@ def logout():
 @hangman_app.route("/play", methods=["GET", "POST"])
 @auth
 def play():
-	if not session.get('remaining_guesses') or session.get('remaining_guesses') == 0:
-		session['answer'] = random_word()
-		session['remaining_guesses'] = session['answer'].possible_guesses
-		session['guessed'] = session['answer'].blanks
-		session['incorrectly_guessed'] = ''
-		session['answer'] = session['answer'].word
-		return render_template('play.html', answer=session['answer'], 
-			remaining_guesses=session['remaining_guesses'], 
-			guessed=session['guessed'], 
-			incorrectly_guessed=session['incorrectly_guessed']
+	if not session.get('game_id'):
+	#if not model.User.get_user(session['username']).active_game():
+		game = model.Game(status='in-progress', username=session['username'])
+		guesses = model.Guesses(game)
+		model.db.session.add(game)
+		model.db.session.add(guesses)
+		model.db.session.commit()
+		session['game_id']=game.id
+		session['guesses_id']=guesses.id
+		return render_template('play.html', answer=guesses.answer, 
+			remaining_guesses=guesses.remaining_guesses, 
+			correct_guesses=guesses.correct_guesses, 
+			incorrect_guesses=guesses.incorrect_guesses
 			)
 
 	if request.method=='GET':
-		return render_template('play.html', answer=session['answer'], 
-			remaining_guesses=session['remaining_guesses'], 
-			guessed=session['guessed'], 
-			incorrectly_guessed=session['incorrectly_guessed']
+		game = model.Game.query.filter_by(id=session['game_id']).first()
+		guesses = model.Guesses.query.filter_by(id=session['guesses_id']).first()
+		return render_template('play.html', answer=guesses.answer, 
+			remaining_guesses=guesses.remaining_guesses, 
+			correct_guesses=guesses.correct_guesses, 
+			incorrect_guesses=guesses.incorrect_guesses
 			)
-	
-	guess = validate_input('guess').lower()
 
-	if len(guess) > 1:
-		flash('Please guess a single letter.')
-		return redirect(url_for('play'))
+	if request.form.get('guess'):
+		if len(request.form.get('guess')) > 1:
+			flash('Please guess a single letter.')
+			return redirect(url_for('play'))
+		elif request.form.get('guess') not in 'abcdefghijklmnopqrstuvwxyz':
+			flash('Please guess a letter, not punction or numbers.')
+			return redirect(url_for('play'))
 
-	if guess not in 'abcdefghijklmnopqrstuvwxyz':
-		flash('Please guess a letter, not punction or numbers.')
-		return redirect(url_for('play'))
-
-	if guess in session['incorrectly_guessed']:
+	guess = request.form.get('guess')
+	guesses = model.Guesses.query.filter_by(id=session['guesses_id']).first()
+	user = model.User.query.filter_by(username=session['username']).first()
+	if guess in guesses.incorrect_guesses:
 		flash('"{}" already guessed. Please guess a new letter.'.format(guess))
 		return redirect(url_for('play'))
-		
-	# move logic in code below 
-		# guess
-		# answer 
-		# score
-			# make the methods from 'verbs'
-			# will make adding features easier
 
-	session['remaining_guesses'] -= 1
+	guesses.process_guess(guess)
+	model.db.session.commit()
+	game = model.Game.query.filter_by(id=session['game_id']).first()
 
-	if guess in session['answer']:
-		session['guessed'] = char_replace(guessed=session['guessed'], guess=guess, answer=session['answer'])
-	else:
-		session['incorrectly_guessed'] += guess
-
-	if "___" not in session['guessed']:
-		add_game('win', session['guessed'], session['answer'], session['username'])
+	if "___" not in guesses.correct_guesses:
+		game.status='win'
+		user.wins += 1
+		model.db.session.commit()
+		del session['game_id']
 		return redirect(url_for('win'))
-	elif session['remaining_guesses'] == 0:
-		add_game('loss', session['guessed'], session['answer'], session['username'])
+	elif guesses.remaining_guesses <= 0:
+		game.status='loss'
+		user.loses += 1
+		model.db.session.commit()
+		del session['game_id']
 		return redirect(url_for('loss'))
 	else:
 		return redirect(url_for('play'))
@@ -157,16 +124,18 @@ def play():
 @hangman_app.route("/win")
 @auth
 def win():
-	if "___" in session['guessed']:
+	if session.get('game_id'):
 		return redirect(url_for('play'))
-	return render_template("win.html", answer=session['answer'])
+	guesses = model.Guesses.query.filter_by(id=session['guesses_id']).first()
+	return render_template("win.html", answer=guesses.answer)
 
 @hangman_app.route("/loss")
 @auth
 def loss():
-	if session.get('remaining_guesses') > 0:
+	if session.get('game_id'):
 		return redirect(url_for('play'))
-	return render_template("loss.html", answer=session['answer'], guessed=session['guessed'])
+	guesses = model.Guesses.query.filter_by(id=session['guesses_id']).first()
+	return render_template("loss.html", answer=guesses.answer, correct_guesses=guesses.correct_guesses)
 
 @hangman_app.route("/scores")
 @auth
--- hangman/model.py
+++ hangman/model.py
@@ -12,36 +12,85 @@ class User(db.Model):
 	password = db.Column(db.String(140), nullable=False)
 	wins = db.Column(db.Integer, default=0)
 	loses = db.Column(db.Integer, default=0)
-	games = db.relationship("Game", backref="user", lazy="dynamic")
 
-	def __init__(self, username, password, wins=0, loses=0):
+	def __init__(self, username, password):
 		self.username=username
 		self.password=password
-		self.wins=wins
-		self.loses=loses
+		self.wins=0
+		self.loses=0
 
 	def __repr__(self):
 		return "User {}".format(self.username)
 
+	@classmethod
+	def get_user(cls, username):
+		return cls.query.filter_by(username=username).first()
+
+	# buggy
+	def active_game(self):
+		games = Game.query.filter_by(username=self.username)
+		most_recent = games.filter_by(status='in-progress').order_by(Game.created_date.desc()).first()
+		return most_recent
+
+	# buggy
+	def latest_game(self):
+		games = Game.query.filter_by(username=self.username).order_by(Game.created_date.desc()).first()
+
+# add one-to-many mapping to user
 class Game(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	created_date = db.Column(db.DateTime, default=datetime.utcnow())
-	status = db.Column(db.String(40))
-	guessed = db.Column(db.String(200))
-	answer = db.Column(db.String(200))
-	remaining_guesses = db.Column(db.Integer)
-	incorrectly_guessed = db.Column(db.String(400))
-	user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+	status = db.Column(db.String(40), nullable=False)
+	answer = db.Column(db.String(200), nullable=False)
+	username = db.Column(db.String(200), nullable=False)
 
-	def __init__(self, status, guessed, answer, remaining_guesses, incorrectly_guessed):
+	def __init__(self, status, username):
+		self.created_date=datetime.utcnow()
 		self.status=status
-		self.guessed=guessed
-		self.answer=answer
-		self.remaining_guesses=remaining_guesses
-		self.incorrectly_guessed=incorrectly_guessed
+		self.answer=Word.random_word()
+		self.username=username
 
 	def __repr__(self):
-		return "Guessed {} which was status {}.".format(self.guessed, self.status)
+		return "{} has status {}.".format(self.username, self.status)
+
+# add one-to-one mapping to game
+class Guesses(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	answer = db.Column(db.String(200), nullable=False)
+	correct_guesses = db.Column(db.String(200))
+	incorrect_guesses = db.Column(db.String(400))
+	remaining_guesses = db.Column(db.Integer, nullable=False)
+	#game_id = db.Column(db.Integer, nullable=False)
+
+	def __init__(self, game):
+		self.answer=game.answer
+		self.correct_guesses=self.get_blanks()
+		self.incorrect_guesses=''
+		self.remaining_guesses=self.possible_guesses()
+		#self.game_id=game.id
+
+	def __repr__(self):
+		return "Progress is {}".format(self.correct_guesses)
+
+	def possible_guesses(self):
+		return len(self.answer)
+
+	def get_blanks(self):
+		return " ".join(["___" for char in self.answer])
+
+	def insert_guess(self, guess):
+		inserted = self.correct_guesses.split(" ")
+		positions = [i for i, char in enumerate(self.answer) if char == guess]
+		for position in positions:
+			inserted[position] = guess
+		self.correct_guesses = " ".join(inserted)
+
+	def process_guess(self, guess):
+		self.remaining_guesses -= 1
+		if guess in self.answer:
+			self.insert_guess(guess)
+		else:
+			self.incorrect_guesses += guess
 
 class Word(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
@@ -53,19 +102,12 @@ class Word(db.Model):
 	def __repr__(self):
 		return self.word
 
-	@property
-	def possible_guesses(self):
-		return len(self.word)
-
-	@property
-	def blanks(self):
-		return " ".join(["___" for char in self.word])
-
 	@classmethod
 	def random_word(cls):
 		num = randint(1, cls.query.count()+1)
-		return cls.query.filter_by(id=num).first()
+		return cls.query.filter_by(id=num).first().word
 
+# db.drop_all()
 db.create_all()
 words = open("hangman/words.txt", "r").read().split("\n")
 for word in words:
