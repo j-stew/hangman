16c79939acfc9b751b92a42dce799150acfe96ca
Jessica Stewart <jessica.stewart@adroll.com>
2015-06-23 13:26:01 -0700

    adding bash script

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!bin/sh
+
+mkvirtualenv hangman_env
+workon hangman_env
+
+pip install -r requirements.txt
+
+export DATABASE_URL=postgres://localhost/hangman
+export DEBUG=True
+export HOST=127.0.0.1
+export PORT=5000
+
+psql -c 'CREATE DATABASE hangman'
+psql -c 'CREATE DATABASE test_hangman'
